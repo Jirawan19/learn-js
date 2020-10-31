c71e6f038ed3e9f60616d22666d8ab4107e8ddf2
in <inclub9@gmail.com>
2020-10-31 19:41:10 +0700

    test.js

---
+++ test.js
@@ -0,0 +1,108 @@
+// console.log("---output---");
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("---output---");
+// console.log("---result---");
+// for (let index = 1; index <= 4; index++) {
+//     console.log(index);    
+// }
+// console.log("---result---");
+// console.log("---output---");
+// console.log("2");
+// console.log("4");
+// console.log("6");
+// console.log("8");
+// console.log("---output---");
+// console.log("---result---");
+// for (let index = 1; index <= 8; index++) {
+//     // console.log(index);
+//     if (index %2==0 ) {
+//         console.log(index);
+//     }    
+// }
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---output---");
+// console.log("1");
+// console.log("3");
+// console.log("5");
+// console.log("7");
+// console.log("---output---");
+// console.log("---result---");
+// for (let index = 1; index <= 7; index++) {
+//     // console.log(index);
+//     if (index %2!=0 ) {
+//         console.log(index);
+//     }    
+// }
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---output---");
+// console.log("1 3 5 7 9 11");
+// console.log("---output---");
+// console.log("---result---");
+// name=""
+// for (let index = 1; index <= 11; index++) {
+//     if (index %2!=0) {
+//       name +=index+" "  
+//     }    
+// }
+// console.log(name);
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---output---");
+// console.log("5 10 15 20 25 30");
+// console.log("---output---");
+// console.log("---result---");
+// name=""
+// for (let index = 5; index <= 30; index=index+5) {
+//     // if (index %5==0) {
+//       name =name+index+" "  
+//     // }    
+// }
+// console.log(name);
+// console.log("---result---");
+// console.log("---result---");
+// console.log("---output---");
+// console.log("1 3 5 7");
+// console.log("---output---");
+// console.log("---result---");
+// name = ""
+// for (let index = 1; index <= 7; index = index + 2) {
+//     name = name + index + " "
+// }
+// console.log(name);
+// console.log("---result---");
+// console.log("---output---");
+// console.log("---output---");
+// console.log("---result---");
+// name = "aaaa" // string
+// name2 = 1 // number
+// name22 = 1.1 //number
+// name3 = [1,2,3] //array => number
+// name4 = ["aaa", "bbb", "ccc"] //array => string
+// name44 = [1, 1.2, "ccc"] //array => string number
+// name5 = {a:"aaaa",b: "bbb", c: [1,2,3]} //oblect
+// console.log(typeof name);
+// console.log(typeof name2);
+// console.log(typeof name22);
+// console.log(typeof name3);
+// console.log(typeof name4);
+// console.log(typeof name5);
+// console.log("---result---");
+console.log("---output---");
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("...");
+console.log("---output---");
+console.log("---result---");
+number = [1, 2, 3, 4, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101, 5, 6, 7, 8, 31, 101]
+number[0] = 100
+console.log(number[number.length - 1]);
+console.log("---result---");
