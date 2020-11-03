5ca836bdd09ea6fe9e43954057803a7ce9aad4fe
in <inclub9@gmail.com>
2020-11-03 21:25:30 +0700

    5/10 point

--- test3.js
+++ test3.js
@@ -1,19 +1,57 @@
 // numbers = [1,2,3,4,5,6]
 // 2 4 6
+// name=""
+// numbers = [1,2,3,4,5,6]
+// for (let index = 0; index < numbers.length; index++) {
+//         if (numbers[index] %2 ==0) {
+//             name = name + numbers[index] + " "
+//         }
+//     }
+// console.log(name);
+
 // ------
 // 1 3 5
+// numbers = [1,2,3,4,5,6]
+// name =""
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] %2 !=0) {
+//         name = name + numbers[index] + " "
+//     }    
+// }
+// console.log(name); 
+
 // ------
 // 2
 // 4
 // 6
+
+// numbers = [1,2,3,4,5,6]
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] %2 ==0) {
+//         console.log(numbers[index]);
+//     }    
+// }
 // ------
 // 1
 // 3
 // 5
+
+// numbers = [1,2,3,4,5,6]
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] %2 != 0) {
+//         console.log(numbers[index]);
+//     }    
+// }
 // ------
 // สูตรคูณแม่ 2 แนวตั้ง
+// for (let index = 1; index <= 12; index++) {
+//     console.log(index + "*" + 2 + "=" + index*2);    
+// }
+
 // ------
-// สูตรคูณแม่ 2 แนวนอน /
+// สูตรคูณแม่ 2 แนวนอน 
+// 2*1=2 3*1=3 4*1=4 ...
+// 2*2=4 3*2=6 4*2=8 ...
 // ------ /
 // 1
 // 1 2
